6f2cc004a61cb6aeef36267812a6b760235806fc
gzukas <gedxaz@gmail.com>
2024-02-01 01:18:24 +0200

    build: tailwind config ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { type Config } from 'tailwindcss';
+
+export default {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -77,4 +78,4 @@ module.exports = {
     }
   },
   plugins: [require('tailwindcss-animate')]
-};
+} satisfies Config;
