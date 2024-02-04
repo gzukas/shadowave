3b7e75172d6b52da10811241ba9c30c33168747f
gzukas <gedxaz@gmail.com>
2024-02-04 19:03:33 +0200

    feat: show theme icons in dropdown

--- src/components/ChangeTheme.tsx
+++ src/components/ChangeTheme.tsx
@@ -1,5 +1,5 @@
 import { useSetAtom } from 'jotai';
-import { Moon, Sun } from 'lucide-react';
+import { Laptop, Moon, Sun } from 'lucide-react';
 import { Trans } from '@lingui/macro';
 import { themeAtom } from '@/atoms/themeAtom';
 import { Button } from '@/components/ui/Button';
@@ -15,9 +15,9 @@ export function ChangeTheme() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="ghost" size="icon" className="h-8 w-8">
+          <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">
             <Trans>Change theme</Trans>
           </span>
@@ -25,12 +25,15 @@ export function ChangeTheme() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme('light')}>
+          <Sun className="mr-2 h-4 w-4" />
           <Trans>Light</Trans>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('dark')}>
+          <Moon className="mr-2 h-4 w-4" />
           <Trans>Dark</Trans>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('system')}>
+          <Laptop className="mr-2 h-4 w-4" />
           <Trans>System</Trans>
         </DropdownMenuItem>
       </DropdownMenuContent>
