fb16ce1ed5e35aab0a59fef30330fd23f8b6e207
gzukas <gedxaz@gmail.com>
2024-02-21 19:34:53 +0200

    fix: remove old prefix from theme key

--- src/atoms/themeAtom.ts
+++ src/atoms/themeAtom.ts
@@ -3,7 +3,7 @@ import { atomWithStorage, createJSONStorage } from 'jotai/utils';
 export type Theme = 'dark' | 'light' | 'system';
 
 export const themeAtom = atomWithStorage<Theme>(
-  'mei-theme',
+  'theme',
   'system',
   createJSONStorage(() => localStorage),
   { getOnInit: true }
