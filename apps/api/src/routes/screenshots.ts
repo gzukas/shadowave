a282a6d7fe2680c08f864263d9635e3654f1d674
Gediminas Zukas <136955+gzukas@users.noreply.github.com>
2025-02-04 22:19:59 +0200

    chore: optimize screenshots

--- apps/api/src/routes/screenshots.ts
+++ apps/api/src/routes/screenshots.ts
@@ -2,12 +2,8 @@ import { Elysia, form, t } from 'elysia';
 import puppeteer, { type Device, KnownDevices } from 'puppeteer';
 import { url, deviceType } from '@workspace/schema';
 
-type ColorScheme = 'light' | 'dark';
 type DeviceType = typeof deviceType.static;
 
-const TIMEOUT = 100000;
-
-const defaultColorSchemes: ColorScheme[] = ['dark', 'light'];
 const deviceMapping: Record<DeviceType, Device> = {
   mobile: KnownDevices['iPhone 15'],
   tablet: KnownDevices['iPad'],
@@ -22,65 +18,53 @@ const deviceMapping: Record<DeviceType, Device> = {
 };
 
 interface ScreenshotsOptions {
+  colorScheme: 'light' | 'dark';
   deviceType?: DeviceType;
-  colorSchemes?: ColorScheme[];
 }
 
-const screenshotService = new Elysia({ name: 'Service.Screenshot' })
-  .decorate('browser', await puppeteer.launch({ headless: 'shell' }))
-  .derive({ as: 'scoped' }, ({ browser }) => ({
-    async *screenshots(url: string, options: ScreenshotsOptions) {
-      const { deviceType = 'desktop', colorSchemes = defaultColorSchemes } =
-        options;
-
-      const page = await browser.newPage();
-      await page.setCacheEnabled(false);
-      await page.emulate(deviceMapping[deviceType]);
-      await page.goto(url, { timeout: TIMEOUT, waitUntil: 'networkidle0' });
-
-      for (const colorScheme of colorSchemes) {
-        await page.emulateMediaFeatures([
-          { name: 'prefers-color-scheme', value: colorScheme }
-        ]);
-        await page.waitForNetworkIdle({ timeout: TIMEOUT });
-        const screenshot = await page.screenshot({
-          type: 'webp'
-        });
-        yield [colorScheme, screenshot] as const;
-      }
-
-      await page.close();
-    }
-  }))
-  .onStop(async ({ decorator: { browser } }) => {
-    await browser.close();
+export const screenshots = async () => {
+  const browser = await puppeteer.launch({
+    headless: 'shell'
   });
 
-export const screenshots = new Elysia().use(screenshotService).get(
-  '/screenshots/:url',
-  async ({ params: { url }, query, screenshots }) => {
-    const formData = form({});
-    for await (const [colorScheme, screenshot] of screenshots(
-      decodeURIComponent(url),
-      query
-    )) {
-      formData.append(
-        colorScheme,
-        new Blob([screenshot]),
-        `${colorScheme}.webp`
-      );
-    }
-    return formData;
-  },
-  {
-    params: t.Object({
-      url
-    }),
-    query: t.Object({
-      deviceType: t.Optional(deviceType)
-    }),
-    transform({ params }) {
-      params.url = decodeURIComponent(params.url);
-    }
-  }
-);
+  const screenshot = async (url: string, options: ScreenshotsOptions) => {
+    const { deviceType = 'desktop', colorScheme } = options;
+    const page = await browser.newPage();
+    await Promise.all([
+      page.emulate(deviceMapping[deviceType]),
+      page.emulateMediaFeatures([
+        { name: 'prefers-color-scheme', value: colorScheme }
+      ])
+    ]);
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const screenshot = await page.screenshot({
+      type: 'webp'
+    });
+    await page.close();
+    return [colorScheme, new Blob([screenshot])] as const;
+  };
+
+  return new Elysia()
+    .get(
+      '/screenshots/:url',
+      async ({ params: { url }, query }) => {
+        const screenshots = await Promise.all([
+          screenshot(url, { ...query, colorScheme: 'dark' }),
+          screenshot(url, { ...query, colorScheme: 'light' })
+        ]);
+        return form(Object.fromEntries(screenshots));
+      },
+      {
+        params: t.Object({
+          url
+        }),
+        query: t.Object({
+          deviceType: t.Optional(deviceType)
+        }),
+        transform({ params }) {
+          params.url = decodeURIComponent(params.url);
+        }
+      }
+    )
+    .onStop(() => browser.close());
+};
